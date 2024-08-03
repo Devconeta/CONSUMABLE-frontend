8fa89a6906f6a6e1fa1cc5e259c87b5b88a21643
nvcho <ipmain07@gmail.com>
2024-08-03 18:04:04 -0300

    add base ui components

---
+++ app/components/ui/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="absolute w-[100vw] h-auto py-4">
+      <ul className="flex justify-center items-center gap-14">
+        <li className="animated-underline">Generate</li>
+        <li className="animated-underline">Consume</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
